69cccad65a2455133c125a622bc6a2a51b259f22
Chris Salch <emeraldd.chris@gmail.com>
2011-06-25 00:50:32 -0500

    Cleaning up some template junk

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 include 'lib/template.class';
 
+$model=array();
+
+// simple selection mode selection logic
+if(isset($_POST['formula'])) {
+    //include 'lib/parser.php';
+    //$result = do_roll($_POST['formula']);
+    $model['formula'] = $_POST['formula'];
+}
 
 $template = new Template('tmpl/index.tmpl');
+$template->title = 'Simple Roller';
 
-$template->title = 'Hello Nurse!';
+$template->set_all($model);
 
 $template->render();
